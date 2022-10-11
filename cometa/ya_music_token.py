e0087bec199a831ad260d8ba9c5260ccd55482a4
Ali Sweetall <imsmastermsk@gmail.com>
2022-10-11 12:54:30 +0300

    Update fetching token

--- cometa/ya_music_token.py
+++ cometa/ya_music_token.py
@@ -7,11 +7,13 @@ def fetch_user_token():
     if os.name == 'nt':
         import winreg            
         subkey = r'htmlfile\shell\open\command'
-        with winreg.OpenKey(winkey.HKEY_CLASSES_ROOT, subkey) as key:
+        with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, subkey) as key:
             dbrowser_path = winreg.QueryValueEx(key, '')[0]
         browser_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
     else:
         browser_path = '/opt/google/chrome/chrome'
+    print(dbrowser_path)
+    print(browser_path)
 
     url = 'https://oauth.yandex.ru/authorize'
     query = '?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d'
@@ -19,10 +21,11 @@ def fetch_user_token():
         browser_path,
         '--enable-logging=stderr',
         url + query,
-        "-v=4",
+        "--v=4",
     ]
     result = subprocess.run(command, capture_output=True)
 
     pattern = r'https://music.yandex.ru/#access_token=(.*?)&'
     return re.search(pattern, result.stderr.decode('utf-8')).group(1)
 
+fetch_user_token()
