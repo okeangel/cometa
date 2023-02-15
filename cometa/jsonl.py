8d77b0c137e772953d190e3c00acc95960a02320
Ali Sweetall <imsmastermsk@gmail.com>
2023-02-15 05:52:57 +0300

    Improve jsonl module performance

--- cometa/jsonl.py
+++ cometa/jsonl.py
@@ -2,37 +2,45 @@ import pathlib
 import json
 
 
-def dump(lines, path, headline=None, mode='w'):
+def fix_suffix(path):
     path = pathlib.Path(path)
     if path.suffix == '':
         path = path.with_suffix('.jsonl')
+    return path
+
+
+def serialize_item(item):
+    return json.dumps(item, ensure_ascii=False, separators=(',', ':'))
+
+
+def dump(items, path, mode='w'):
+    path = fix_suffix(path)
     with open(path, mode, encoding='utf-8') as output_file:
-        if headline:
-            json.dump(headline,
-                      output_file,
-                      ensure_ascii=False,
-                      separators=(',', ':'))
-            output_file.write('\n')
-        for line in lines:
-            json.dump(line,
-                      output_file,
-                      ensure_ascii=False,
-                      separators=(',', ':'))
-            output_file.write('\n')
+        records = map(
+            lambda item: serialize_item(item) + '\n',
+            items,
+        )
+        output_file.writelines(records)
+
+
+def extend(items, path):
+    dump(items, path, mode='a')
+
+
+def append(item, path):
+    path = fix_suffix(path)
+    with open(path, 'a', encoding='utf-8') as output_file:
+        output_file.write(serialize_item(item) + '\n')
 
 
 def load(path):
-    path = pathlib.Path(path)
-    if path.suffix == '':
-        path = path.with_suffix('.jsonl')
+    path = fix_suffix(path)
     with open(path, encoding='utf-8') as input_file:
         return [json.loads(line) for line in input_file]
 
 
-def load_by_lines(path):
-    path = pathlib.Path(path)
-    if path.suffix == '':
-        path = path.with_suffix('.jsonl')
+def items_of(path):
+    path = fix_suffix(path)
     with open(path, encoding='utf-8') as input_file:
         for line in input_file:
             yield json.loads(line)
